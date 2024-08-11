0c3019d8ef56374c1a9ebbe76beb0bd40f60135a
suemor233 <suemor233@outlook.com>
2024-08-11 23:00:07 +0800

    feat: eslint config

---
+++ electron.vite.config.ts
@@ -0,0 +1,21 @@
+import { resolve } from 'node:path'
+
+import react from '@vitejs/plugin-react'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src'),
+      },
+    },
+    plugins: [react()],
+  },
+})
---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+import { defineConfig } from 'eslint-config-hyoban'
+
+export default defineConfig(
+  {
+    formatting: {
+      quotes: 'single',
+      arrowParens: true,
+      braceStyle: '1tbs',
+      lineBreak: 'after',
+    },
+    lessOpinionated: true,
+    ignores: ['src/renderer/src/hono.ts', 'src/hono.ts'],
+    preferESM: false,
+  },
+  {
+    settings: {
+      tailwindcss: {
+        whitelist: ['center'],
+      },
+    },
+    rules: {
+      'unicorn/prefer-math-trunc': 'off',
+      'no-restricted-globals': [
+        'error',
+        {
+          name: 'location',
+          message:
+            'Since you don\'t use the same router instance in electron and browser, you can\'t use the global location to get the route info. \n\n' +
+            'You can use `useLocaltion` or `getReadonlyRoute` to get the route info.',
+        },
+      ],
+    },
+  },
+)
