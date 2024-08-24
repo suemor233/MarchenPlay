0cf9f5c9b4fa95111cfac84979b91e283cc8fd27
suemor233 <suemor233@outlook.com>
2024-08-24 23:44:20 +0800

    feat: danmu.js

--- src/renderer/src/components/modules/Player/index.tsx
+++ src/renderer/src/components/modules/Player/index.tsx
@@ -1,6 +1,6 @@
 import type { FC } from 'react'
 import { useEffect, useRef } from 'react'
-import XgPlayer from 'xgplayer'
+import XgPlayer, { Danmu } from 'xgplayer'
 
 interface PlayerProps {
   url: string
@@ -19,6 +19,31 @@ export const Player: FC<PlayerProps> = (props) => {
         lang: 'zh',
         autoplay: true,
         volume: 1,
+        miniprogress: true,
+        screenShot: true,
+        pip: true,
+
+        plugins: [Danmu],
+        danmu: {
+          comments: [
+            {
+              duration: 15000, // 弹幕持续显示时间,毫秒(最低为5000毫秒)
+              id: '1', // 弹幕id，需唯一
+              start: 1000, // 弹幕出现时间，毫秒
+              txt: '长弹幕长弹幕长弹幕长弹幕长弹幕长弹幕长弹幕长弹幕长弹幕长弹幕', // 弹幕文字内容
+              mode: 'top',
+              style: {
+                color: '#fff',
+              },
+            },
+          ],
+          fontSize: 25,
+          ext: {
+            mouseControl: true,
+            mouseControlPause: true,
+          },
+
+        },
       })
     }
   }, [playerRef, url])
