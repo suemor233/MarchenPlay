22a8a13373950012903e9aa9d64571700c784428
suemor233 <suemor233@outlook.com>
2024-09-05 01:56:27 +0800

    feat: narrow the import click range

--- src/renderer/src/page/player/index.tsx
+++ src/renderer/src/page/player/index.tsx
@@ -2,19 +2,22 @@ import { Player } from '@renderer/components/modules/player'
 import { useVideo } from '@renderer/components/modules/player/hooks'
 import { cn } from '@renderer/libs/utils'
 import { VideoProvider } from '@renderer/providers/player/PlayerProvider'
+import type { FC } from 'react'
 import { useMemo, useRef } from 'react'
 
 export default function VideoPlayer() {
   const { handleDragOver, handleNewVideo, url, showAddVideoTips } = useVideo()
   const fileInputRef = useRef<HTMLInputElement | null>(null)
-  const content = useMemo(() => url ? <Player url={url} /> : <DragTips />, [url])
+
+  const clickImportvideo = () => fileInputRef.current?.click()
+  const content = useMemo(() => url ? <Player url={url} /> : <DragTips onClick={clickImportvideo} />, [url])
+
   return (
     <VideoProvider>
       <div
         onDrop={handleNewVideo}
         onDragOver={handleDragOver}
-        onClick={() => fileInputRef.current?.click()}
-        className={cn('flex size-full items-center justify-center ', showAddVideoTips && 'cursor-pointer')}
+        className={cn('flex size-full items-center justify-center ')}
       >
         {content}
         {showAddVideoTips && (
@@ -31,8 +34,8 @@ export default function VideoPlayer() {
   )
 }
 
-const DragTips = () => (
-  <div className="flex flex-col items-center gap-2 text-gray-500">
+const DragTips: FC<{ onClick: () => void }> = ({ onClick }) => (
+  <div className="flex cursor-pointer flex-col items-center gap-2 p-32 text-gray-500" onClick={onClick}>
     <i className="icon-[mingcute--video-line] text-6xl " />
     <p className="text-xl">点击或拖拽动漫到此处播放</p>
   </div>
